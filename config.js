e76bbdc30e42888465da80ba978413a2f3ee764a
ivanrincon2 <ivandariorincon.25@gmail.com>
2025-07-28 21:49:01 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
